10d510226de2a7709c8ed93b6ad392f7d6ba3fb8
chintoz <jacintomena@gmail.com>
2023-01-26 23:47:26 +0100

    First changes in react app

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="http://photo1419.es/wp-content/uploads/2020/09/photo-1419-blanco-1.png" className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Hola me llamo Jacinto.
+        </p>        
+        <p>
+          <p id="Patri">
+            Edit <code>src/App.js</code> and save to reload. Hola me llamo Patri. 
+          </p>
+          <p id="Jacin">
+            Edit <code>src/App.js</code> and save to reload. Hola me llamo Jacin.
+          </p>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vete al google
         </a>
       </header>
-    </div>
+      
+    </div>    
   );
 }
 
